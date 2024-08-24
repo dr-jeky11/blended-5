7df20015cd8922f6ed6eadfa9a1d7d0652b19908
dr-jeky11 <tyrsyna@gmail.com>
2024-08-24 14:26:06 +0200

    query

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,15 +1,17 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_ENDPOINT = '/comments';
-const BASE_URL = '';
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://66c9cbe359f4350f064d77ae.mockapi.io";
 
 export const commentApi = createApi({
-  reducerPath: 'comments',
+  reducerPath: "comments",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
-  tagTypes: ['Comments'],
-  endpoints: (builder) => ({}),
+  tagTypes: ["Comments"],
+  endpoints: (builder) => ({
+    getComments: builder.query({ query: () => API_ENDPOINT }),
+  }),
 });
 
-export const {} = commentApi;
+export const {useGetCommentsQuery} = commentApi;
